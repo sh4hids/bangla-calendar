326f7e805f768e48ac536af4be63f04e2ea69e72
sh4hids <dev@shahid.pro>
2021-08-12 08:47:14 +0600

    ↕ update packages

--- esm/getWeekDay.js
+++ esm/getWeekDay.js
@@ -1,1 +1,1 @@
-import{i as e,e as t,b as a}from"./utils-d28c699f.js";export default function(r=new Date,n={}){return e(r)?function(e=0,t="eeee"){switch(t){case"eee":return a[e];default:return a[e]+"বার"}}((r=new Date(r)).getDay(),n.format||"eeee"):t}
+import{i as e,e as t,b as r}from"./utils-d28c699f.js";function a(a=new Date,n={}){if(!e(a))return t;return function(e=0,t="eeee"){switch(t){case"eee":return r[e];default:return`${r[e]}বার`}}((a=new Date(a)).getDay(),n.format||"eeee")}export{a as default};
